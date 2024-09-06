2475ca99be4406e095f2cb3eeb609ae4bf9419a9
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-06 11:00:01 +0600

    previous practice end and fundamental function practice start

---
+++ loopTask.js
@@ -0,0 +1,49 @@
+// Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+//solution
+
+/*for (let i = 1; i <= 200; i++) {
+	console.log(i);
+	if (i >= 100){
+		break;
+	}
+
+}
+	*/
+
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+// let i = 1;
+// let sum = 0;
+
+// while (i < 20){
+// 	console.log(i);
+// 	sum = sum + i;
+// 	console.log(sum);
+// 	i++;
+
+
+// }
+
+// for (i = 81; i <= 131; i++){
+// 	if(i % 2  === 1){
+// 		console.log(i);
+// 	}
+// 	// console.log(i);
+// }
+// for (i = 61; i <= 100; i+=1)
+
+
+// let i = 81;
+// while (i <= 131)  {
+// 	console.log(i);
+// 	i+=2;
+// }
+
+// let i = 206;
+// while (i < 311) {
+// 	console.log(i);
+// 	i+=2;
+// }
