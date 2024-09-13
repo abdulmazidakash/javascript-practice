a28b36b9fec3a3bf1ed67d037334a6f8017cbde8
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-14 00:13:43 +0600

    problem solving

--- problemSolving.js
+++ problemSolving.js
@@ -136,3 +136,60 @@ console.log(deleteInvalids(["1" , {num:2} , NaN ]));
 console.log(deleteInvalids([ 1 , 2 , -3 ] ));
 console.log(deleteInvalids({num: [ 1 , 2 , 3 ]} ));
 
+//problem 4
+
+
+function password(obj) {
+
+
+	if (obj.name === undefined || obj.birthYear === undefined || obj.siteName === undefined || obj.birthYear.toString().length !== 4){
+		return "invalid";
+	}
+
+	let capitalSiteName = obj.siteName[0].toUpperCase() + obj.siteName.slice(1);
+	
+	let result = capitalSiteName + '#' + obj.name + '@' + obj.birthYear;
+
+	return result ;
+}
+
+console.log(password({ name: "kolimuddin" , birthYear: 1999 , siteName: "google" }));
+console.log(password({ name: "rahat", birthYear: 2002, siteName: "Facebook" } ));
+console.log(password({ name: "toky", birthYear: 200, siteName: "Facebook" }));
+console.log(password({ name: "maisha", birthYear: 2002 } ));
+
+
+//problem 5
+
+function monthlySavings(arr , livingCost) {
+
+	if (!Array.isArray(arr) && typeof livingCost !== 'number'){
+		return "Invalid Input";
+	}
+	
+	let totalIncome = 0;
+	for (let payment of arr){
+		// console.log(payment);
+		if (payment >= 3000){
+			let  tax = payment * 0.2;
+			totalIncome += payment - tax;
+		}else{
+			totalIncome += payment;
+		}
+
+	}
+
+	let savings = totalIncome - livingCost;
+	if (savings < 0){
+		return 'earn more';
+	}
+	return savings;
+
+}
+	
+
+console.log(monthlySavings([ 1000 , 2000 , 3000 ] , 5400 ));
+console.log(monthlySavings([ 1000 , 2000 , 2500 ] , 5000 ));
+console.log(monthlySavings([ 900 , 2700 , 3400] , 10000 ));
+console.log(monthlySavings(100, [ 900 , 2700 , 3400] ));
+	
